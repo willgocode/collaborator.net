3eeffac776b1a700a58dc96b29b5807d42805bc3
William Wu <williamwu0013@gmail.com>
2016-03-24 13:46:55 -0400

    Changed variable names to make more sense. Added in extra lines to show file input.

--- generateDB.py
+++ generateDB.py
@@ -4,17 +4,22 @@ import pandas as pd
 import sys, getopt, pprint
 from pymongo import MongoClient
 
-csvfile = open('test.csv', 'r')
+print('Storing users')
+csvfile = open('users.csv', 'r')
 client = MongoClient('localhost', 27017)
-db = client['test-database']
-collection_name = 'test_collection'
-collection = db.collection_name
+db = client['collaboratornet']
+collection = db.Users
 data = pd.read_csv(csvfile)
 data_json = json.loads(data.to_json(orient = 'records'))
 collection.remove()
 collection.insert(data_json)
 
-cursor = db.collection_name.find()
+print('Reading: organizations')
 
-for document in cursor:
-	print(document)
+print('Reading: projects')
+
+print('Reading: skills')
+
+print('Reading: interests')
+
+print('Reading: distances')
