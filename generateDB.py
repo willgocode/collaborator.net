5d45327bc8ac54bb66fb23ddc87047c98f4bf543
William Wu <williamwu0013@gmail.com>
2016-03-30 17:10:29 -0400

    Added command line parsing.

--- generateDB.py
+++ generateDB.py
@@ -4,20 +4,48 @@ import pandas as pd
 import sys, getopt, pprint
 import helpers
 from pymongo import MongoClient 
- 
-client = MongoClient('localhost', 27017)
-db = client['collaboratornet']
 
-print('Reading: users')
-helpers.storeFile('users.csv', db)
+def main(argv): 
+	usersFile = ''
+	organizationsFile = ''
+	projectFile = ''
+	skillsFile = ''
+	interestsFile = ''
+	distancesFile = ''
 
-print('Reading: organizations')
-helpers.updateOrganization('organizations.csv', db)
+	if len(argv) == 0:
+		print 'Error: Not enough arguments. Type "python generateDB.py -h" for help.'
+		sys.exit()
+	
+	client = MongoClient('localhost', 27017)
+	db = client['collaboratornet']	
 
-print('Reading: projects')
+	try:
+		opts, args = getopt.getopt(argv, "h", ["users="])
+#, "organizations=", "projects=", 
+#						"skills=", "interests=", "distances="])
+	except getopt.GetoptError:
+		sys.exit()
+	
+	for opt, arg in opts:
+		if opt == '-h':
+			print 'Run program with "python generateDB.py <users>.csv <organizations>.csv <projects>.csv <skills>.csv <interests>.csv <distances>.csv".'
+			sys.exit()
 
-print('Reading: skills')
+	print('Reading: users')
+	usersFile = sys.argv[1]
+	helpers.storeFile(usersFile, db)
 
-print('Reading: interests')
+#	print('Reading: organizations')
+#	helpers.updateOrganization('organizations.csv', db)
 
-print('Reading: distances')
+#	print('Reading: projects')
+
+#	print('Reading: skills')
+
+#	print('Reading: interests')
+
+#	print('Reading: distances')
+
+if __name__ == "__main__":
+	main(sys.argv[1:])
