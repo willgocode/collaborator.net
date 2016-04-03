3bdc07ec326a631c168a23b0a14c21dcb6637036
William Wu <williamwu0013@gmail.com>
2016-04-03 09:57:51 -0400

    Added design document for MongoDB. mongoQuery updated to support interest level queries.

--- mongoQuery.py
+++ mongoQuery.py
@@ -2,13 +2,9 @@ import sys, getopt
 from pymongo import MongoClient
 
 def main(argv):
-	fileList = [ '', '', '', '']
+	fileList = [ '', '', '', '', '', '']
 	query = {}
 	userName = ''
-#	organizationName = ''
-#	projectName = ''
-#	interestName = ''
-#	skillName = ''
 
 	client = MongoClient('localhost', 27017)
 	db = client['collaboratornet']
@@ -21,8 +17,8 @@ def main(argv):
 			print i
 			print '\n' 
 	try:
-		opts, args = getopt.getopt(argv, "hu:o:p:i:s:",["userName=", "organizationName=", 
-					"projectName=", "interestName=", "skillList="])
+		opts, args = getopt.getopt(argv, "hu:o:p:i:k:s:t:",["userName=", "organizationName=", 
+					"projectName=", "interestName=", "interestLevel=", "skillList=", "skillLevel="])
 
 	except getopt.GetoptError:
 		print 'See "python mongoQuery.py -h" for help on running program.'
@@ -35,7 +31,9 @@ def main(argv):
 			print '-o = query for organization \n'
 			print '-p = query for project \n'
 			print '-i = query for interest \n'
+			print '-k = query for interest level\n'
 			print '-s = query for skill \n'
+			print '-t = query for skill level \n'
 			print 'You may use any combination of these.\n'
 			print 'Example:\n'
 			print 'python mongoQuery -u <userName> -o <organizationName> \n'
@@ -46,18 +44,32 @@ def main(argv):
 			userName = arg.split()
 			query['first_name'] = userName[0]
 			query['last_name'] = userName[1]
+
 		elif opt in ("-o", "--organizationName"):
 			fileList[0] = arg
 			query['organization'] = fileList[0]
+
 		elif opt in ("-p", "--projectName"):
 			fileList[1] = arg
 			query['project'] = fileList[1]
+
 		elif opt in ("-i", "--interestName"):
 			fileList[2] = arg
 			query['interest'] = fileList[2]
-		elif opt in ("-s", "skillName"):
+
+		elif opt in ("-k", "--interestLevel"):
 			fileList[3] = arg
-			query['skill'] = fileList[3]
+			to_int = int(fileList[3])
+			query['interest_level'] = to_int
+
+		elif opt in ("-s", "--skillList"):
+			fileList[4] = arg
+			query['skill'] = fileList[4]
+
+		elif opt in ("-t", "--skillLevel"):
+			fileList[5] = arg
+			to_int = int(fileList[5])
+			query['skill_level'] = to_int
 
 	cursor = collection.find( query )
 	for i in cursor:
